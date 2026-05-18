f668d6cf901f038f7838903300d992dffcac231e
Md Mahedi Hasan <mdmahedi6427@gmail.com>
2026-05-18 06:50:27 +0600

    create citizen dashboard overview statistics

---
+++ main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { router } from './routes/Routes.jsx'
+import { RouterProvider } from 'react-router'
+import AuthProvider from './context/AuthProvider.jsx'
+import { ToastContainer } from 'react-toastify'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ToastContainer />
+        <RouterProvider router={router}></RouterProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>,
+)
